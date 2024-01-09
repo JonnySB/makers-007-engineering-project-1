bdb3d61e4cebe4d2dda351dc0bb221915a65b43c
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-09 14:52:20 +0000

    eq

--- lib/booking.py
+++ lib/booking.py
@@ -5,5 +5,13 @@ class Booking:
         self.available = available
         self.space_id = space_id
 
+    # ensure two object with identical attrs will be found equal
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    # return nicely formatted string version of peep object
+    def __repr__(self):
+        return f"Peep({self.id}, {self.message}, {self.date_created}, {self.user_id})"
+
     def book(self):
         pass
--- tests/test_booking.py
+++ tests/test_booking.py
@@ -5,7 +5,7 @@ from datetime import datetime
 Initialises with given attributes
 """
 
-def test_initialises_with_a_given_length():
+def test_initialises_with_given_attributes():
     date = Booking(1, datetime(2023, 12, 5), True, 1)
     assert date.id == 1
     assert date.date == datetime(2023, 12, 5)
@@ -13,3 +13,7 @@ def test_initialises_with_a_given_length():
     assert date.space_id == 1
 
 
+def test_booking_eq():
+    date1 = Booking(1, datetime(2023, 12, 5), True, 1)
+    date2 = Booking(1, datetime(2023, 12, 5), True, 1)
+    assert date1 == date2
