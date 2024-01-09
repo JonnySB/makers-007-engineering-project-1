34aff73cf2b2d4f9f411d25ff05a91fd6fe62b7b
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-01-09 16:55:18 +0000

    Create routes and spaces.html template

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,20 @@
 from playwright.sync_api import Page, expect
 
 # Tests for your routes go here
-
+"""
+When I call GET /spaces
+I get a list of spaces back
+"""
+def test_get_spaces(page, test_web_address, db_connection):
+    db_connection.seed("seeds/makers_bnb.sql")
+    page.goto(f"http://{test_web_address}/spaces")
+    name_element = page.locator(".t-space-name")
+    expect(name_element).to_have_text(["Space1", "Space2", "Space3", "Space4", "Space5"])
+    description_element = page.locator(".t-space-description")
+    expect(description_element).to_have_text(["Example description 1", 
+                                            "Example description 2", 
+                                            "Example description 3", 
+                                            "Example description 4", 
+                                            "Example description 5"])
+    price_element = page.locator(".t-space-price")
+    expect(price_element).to_have_text(["£130.0" for _ in range(5)])
