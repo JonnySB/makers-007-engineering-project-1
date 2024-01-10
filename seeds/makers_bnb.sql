6f735e2537b4f28ffc261664a90b344f8bd5b47c
Pier Bruno Pompilii <pier340@gmail.com>
2024-01-10 09:50:27 +0000

    story 1

--- seeds/makers_bnb.sql
+++ seeds/makers_bnb.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP SEQUENCE IF EXISTS users_id_seq;
 
 CREATE SEQUENCE IF NOT EXISTS users_id_seq;
@@ -10,7 +11,7 @@ CREATE TABLE users (
 
 ----------------
 
-DROP TABLE IF EXISTS spaces;
+DROP TABLE IF EXISTS spaces CASCADE;
 DROP SEQUENCE IF EXISTS spaces_id_seq;
 
 CREATE SEQUENCE IF NOT EXISTS spaces_id_seq;
@@ -27,7 +28,7 @@ CREATE TABLE spaces (
 
 ----------------
 
-DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS bookings CASCADE;
 DROP SEQUENCE IF EXISTS bookings_id_seq;
 
 CREATE SEQUENCE IF NOT EXISTS bookings_id_seq;
