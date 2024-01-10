e9f6ca0367c7730b8a5d8035a655411cd66685a4
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-01-10 16:19:56 +0000

    List a space functionality complete except still needs dates and user_id incorporated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
 from lib.space_repository import *
 from lib.space import *
@@ -14,7 +14,22 @@ def get_spaces():
     connection = get_flask_database_connection(app)
     space_repo = SpaceRepository(connection)
     spaces = space_repo.all()
-    return render_template("spaces.html", spaces=spaces)
+    return render_template("spaces/spaces.html", spaces=spaces)
+
+@app.route("/spaces/new", methods=['GET'])
+def get_new_space():
+    return render_template('spaces/new.html')
+
+@app.route("/spaces", methods=['POST'])
+def create_space():
+    connection = get_flask_database_connection(app)
+    space_repository = SpaceRepository(connection)
+    name = request.form['name']
+    description = request.form['description']
+    price = request.form['price']
+    space = Space(None, name, description, price, None)
+    space = space_repository.create(space)
+    return redirect(f"/spaces")
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
--- tests/test_app.py
+++ tests/test_app.py
@@ -18,3 +18,28 @@ def test_get_spaces(page, test_web_address, db_connection):
                                             "Example description 5"])
     price_element = page.locator(".t-space-price")
     expect(price_element).to_have_text(["£130.0" for _ in range(5)])
+
+"""
+When we create a new space
+We see it in the spaces index
+"""
+def test_create_space(page, test_web_address, db_connection):
+    db_connection.seed("seeds/makers_bnb.sql")
+    page.goto(f"http://{test_web_address}/spaces")
+    page.click("text=List a space")
+    page.fill("input[name='name']", "Test Name")
+    page.fill("input[name='description']", "Test Description")
+    page.fill("input[name='price']", "100")
+    # When log in functionality is developed, must include test for correct user_id
+    page.click("text=List my space")
+    name_element = page.locator(".t-space-name")
+    expect(name_element).to_have_text(["Space1", "Space2", "Space3", "Space4", "Space5", "Test Name"])
+    description_element = page.locator(".t-space-description")
+    expect(description_element).to_have_text(["Example description 1", 
+                                            "Example description 2", 
+                                            "Example description 3", 
+                                            "Example description 4", 
+                                            "Example description 5",
+                                            "Test Description"])
+    price_element = page.locator(".t-space-price")
+    expect(price_element).to_have_text(["£130.0" for _ in range(5)] + ["£100.0"])
